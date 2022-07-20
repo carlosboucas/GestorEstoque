a353e2281d407c517906b8c9af9bd4ddc91f5548
carlosboucas <carlos.rodrigo.cmb@gmail.com>
2022-07-20 06:48:54 -0300

    commit inicial

---
+++ GestorEstoque/IEstoque.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorEstoque
+{
+    interface IEstoque
+    {
+        void Exibir();
+        void AdicionarEntrada(); //add novo item ao estoque ou nova vaga.
+        void AdicionarSaida(); //Registrar vendas.
+    }
+}
